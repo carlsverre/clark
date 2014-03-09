de1a28dc48033c9fca401aedfabbdb366e6b24e9
Carl Sverre <carl@carlsverre.com>
2014-03-09 16:43:57 -0700

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
 # get version
-from memsql import __version__
+from superpy import __version__
 
 class PyTest(TestCommand):
     user_options = [
